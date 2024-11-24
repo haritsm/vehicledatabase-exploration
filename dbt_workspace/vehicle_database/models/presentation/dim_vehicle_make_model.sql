6cff92aa8932d2759ecbd40e6c97f753bffc0e6f
haritsm <haritsmuhammad94@gmail.com>
2024-11-24 13:00:00 +0800

    update 20241124

--- dbt_workspace/vehicle_database/models/presentation/dim_vehicle_make_model.sql
+++ dbt_workspace/vehicle_database/models/presentation/dim_vehicle_make_model.sql
@@ -1,27 +1,24 @@
 
-/*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
-
-    Try changing "table" to "view" below
-*/
-
 {{ config(materialized='table') }}
 
 with source_data as (
+    select 
+        make
+        , model
+        , current_localtimestamp() as created_timestamp
+        , current_localtimestamp() as updated_timestamp 
+    from 
+        vehicle_year_make_model_value
+),
 
-    select 1 as id
-    union all
-    select null as id
-
+add_hash as (
+    select 
+        distinct 
+        hash(make || '-' || model || '-' || strftime(created_timestamp, '%Y-%m-%d %H:%M:%S') || '-' || strftime(updated_timestamp, '%Y-%m-%d %H:%M:%S')) AS car_id
+        , *
+    from 
+        source_data
 )
 
 select *
-from source_data
-
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
-
--- where id is not null
+from add_hash
